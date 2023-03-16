465977216aae7eb5cca0b926ff5ee5597a5c1952
Duong Dinh Hai <duonggiaothien@gmail.com>
2023-03-16 23:06:41 +0700

    reset sql by neWKing1

--- sql/da1.sql
+++ sql/da1.sql
@@ -1,170 +1,288 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 16, 2023 lúc 05:06 PM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
 
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 100427
- Source Host           : localhost:3306
- Source Schema         : da1
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100427
- File Encoding         : 65001
 
- Date: 16/03/2023 23:04:43
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Cơ sở dữ liệu: `da1`
+--
 
--- ----------------------------
--- Table structure for bill
--- ----------------------------
-DROP TABLE IF EXISTS `bill`;
-CREATE TABLE `bill`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `total_price` decimal(10, 0) NULL DEFAULT NULL,
-  `status` int NULL DEFAULT 0 COMMENT '0 là đang chờ xác nhận',
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `total_price` decimal(10,0) DEFAULT NULL,
+  `status` int(11) DEFAULT 0 COMMENT '0 là đang chờ xác nhận',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `bill_detail`
+--
+
+CREATE TABLE `bill_detail` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `variation_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `price` decimal(10,0) UNSIGNED DEFAULT NULL,
+  `quantity` int(10) UNSIGNED DEFAULT NULL,
+  `unit_price` decimal(10,0) UNSIGNED DEFAULT NULL,
+  `bill_id` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`) VALUES
+(2, 'Casio4'),
+(3, 'cá'),
+(4, 'cá');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `color`
+--
+
+CREATE TABLE `color` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `color`
+--
+
+INSERT INTO `color` (`id`, `name`) VALUES
+(1, 'hồng'),
+(2, 'tím');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `detail` longtext DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+  `brand_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- ----------------------------
--- Records of bill
--- ----------------------------
-BEGIN;
+--
+-- Đang đổ dữ liệu cho bảng `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `image`, `description`, `detail`, `created_at`, `updated_at`, `brand_id`) VALUES
+(6, 'Dương đẹp trai lọ 2', '6413312983911-casio-ltp-vt01gl-9budf-nu-1.jpg', '<p>1</p>\r\n', '<p>1</p>\r\n', '2023-03-16 09:08:45', '2023-03-16 09:09:29', 2),
+(7, 'Dương đẹp trai lọ 1', '64133136a19b9-casio-mtp-e715d-7avdf-nam-1.jpg', '<p>2</p>\r\n', '<p>2</p>\r\n', '2023-03-16 09:09:42', '2023-03-16 09:36:25', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `size`
+--
+
+CREATE TABLE `size` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `size`
+--
+
+INSERT INTO `size` (`id`, `name`) VALUES
+(1, '32mn'),
+(2, '34mn');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `variation`
+--
+
+CREATE TABLE `variation` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `color_id` bigint(20) UNSIGNED NOT NULL,
+  `size_id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `quantity` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `variation`
+--
+
+INSERT INTO `variation` (`id`, `color_id`, `size_id`, `product_id`, `price`, `discount`, `quantity`) VALUES
+(18, 2, 2, 7, '1', 1, 1);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `variation_id` (`variation_id`),
+  ADD KEY `bill_id` (`bill_id`);
+
+--
+-- Chỉ mục cho bảng `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `color`
+--
+ALTER TABLE `color`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `brand_id` (`brand_id`);
+
+--
+-- Chỉ mục cho bảng `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `variation`
+--
+ALTER TABLE `variation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `color_id` (`color_id`),
+  ADD KEY `size_id` (`size_id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `color`
+--
+ALTER TABLE `color`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `size`
+--
+ALTER TABLE `size`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `variation`
+--
+ALTER TABLE `variation`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  ADD CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`variation_id`) REFERENCES `variation` (`id`),
+  ADD CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`id`);
+
+--
+-- Các ràng buộc cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`);
+
+--
+-- Các ràng buộc cho bảng `variation`
+--
+ALTER TABLE `variation`
+  ADD CONSTRAINT `variation_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `variation_ibfk_2` FOREIGN KEY (`color_id`) REFERENCES `color` (`id`),
+  ADD CONSTRAINT `variation_ibfk_3` FOREIGN KEY (`size_id`) REFERENCES `size` (`id`);
 COMMIT;
 
--- ----------------------------
--- Table structure for bill_detail
--- ----------------------------
-DROP TABLE IF EXISTS `bill_detail`;
-CREATE TABLE `bill_detail`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `variation_id` bigint UNSIGNED NULL DEFAULT NULL,
-  `price` decimal(10, 0) UNSIGNED NULL DEFAULT NULL,
-  `quantity` int UNSIGNED NULL DEFAULT NULL,
-  `unit_price` decimal(10, 0) UNSIGNED NULL DEFAULT NULL,
-  `bill_id` bigint UNSIGNED NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `variation_id`(`variation_id` ASC) USING BTREE,
-  INDEX `bill_id`(`bill_id` ASC) USING BTREE,
-  CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`variation_id`) REFERENCES `variation` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- ----------------------------
--- Records of bill_detail
--- ----------------------------
-BEGIN;
-COMMIT;
-
--- ----------------------------
--- Table structure for brands
--- ----------------------------
-DROP TABLE IF EXISTS `brands`;
-CREATE TABLE `brands`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- ----------------------------
--- Records of brands
--- ----------------------------
-BEGIN;
-INSERT INTO `brands` (`id`, `name`) VALUES (2, 'Casio4'), (3, 'cá'), (4, 'cá');
-COMMIT;
-
--- ----------------------------
--- Table structure for color
--- ----------------------------
-DROP TABLE IF EXISTS `color`;
-CREATE TABLE `color`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- ----------------------------
--- Records of color
--- ----------------------------
-BEGIN;
-INSERT INTO `color` (`id`, `name`) VALUES (1, 'hồng'), (2, 'tím');
-COMMIT;
-
--- ----------------------------
--- Table structure for products
--- ----------------------------
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE `products`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
-  `detail` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `brand_id` bigint UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `brand_id`(`brand_id` ASC) USING BTREE,
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- ----------------------------
--- Records of products
--- ----------------------------
-BEGIN;
-INSERT INTO `products` (`id`, `name`, `image`, `description`, `detail`, `created_at`, `updated_at`, `brand_id`) VALUES (6, 'Dương đẹp trai lọ 2', '6413312983911-casio-ltp-vt01gl-9budf-nu-1.jpg', '<p>1</p>\r\n', '<p>1</p>\r\n', '2023-03-16 16:08:45', '2023-03-16 16:09:29', 2), (7, 'Dương đẹp trai lọ 1', '64133136a19b9-casio-mtp-e715d-7avdf-nam-1.jpg', '<p>2</p>\r\n', '<p>2</p>\r\n', '2023-03-16 16:09:42', '2023-03-16 16:36:25', 3);
-COMMIT;
-
--- ----------------------------
--- Table structure for size
--- ----------------------------
-DROP TABLE IF EXISTS `size`;
-CREATE TABLE `size`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- ----------------------------
--- Records of size
--- ----------------------------
-BEGIN;
-INSERT INTO `size` (`id`, `name`) VALUES (1, '32mn'), (2, '34mn');
-COMMIT;
-
--- ----------------------------
--- Table structure for variation
--- ----------------------------
-DROP TABLE IF EXISTS `variation`;
-CREATE TABLE `variation`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `color_id` bigint UNSIGNED NOT NULL,
-  `size_id` bigint UNSIGNED NOT NULL,
-  `product_id` bigint UNSIGNED NOT NULL,
-  `price` decimal(10, 0) NULL DEFAULT NULL,
-  `discount` int NULL DEFAULT NULL,
-  `quantity` int UNSIGNED NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `product_id`(`product_id` ASC) USING BTREE,
-  INDEX `color_id`(`color_id` ASC) USING BTREE,
-  INDEX `size_id`(`size_id` ASC) USING BTREE,
-  CONSTRAINT `variation_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `variation_ibfk_2` FOREIGN KEY (`color_id`) REFERENCES `color` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `variation_ibfk_3` FOREIGN KEY (`size_id`) REFERENCES `size` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- ----------------------------
--- Records of variation
--- ----------------------------
-BEGIN;
-INSERT INTO `variation` (`id`, `color_id`, `size_id`, `product_id`, `price`, `discount`, `quantity`) VALUES (18, 2, 2, 7, 1, 1, 1);
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
