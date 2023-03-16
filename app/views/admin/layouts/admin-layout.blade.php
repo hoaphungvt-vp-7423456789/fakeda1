8c48c163969d0431ed57799c75b32d72c92d19b0
Duong Dinh Hai <duonggiaothien@gmail.com>
2023-03-16 14:24:18 +0700

    success product

--- app/views/admin/layouts/admin-layout.blade.php
+++ app/views/admin/layouts/admin-layout.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="//cdn.ckeditor.com/4.20.2/full/ckeditor.js"></script>
 </head>
 
 <body>
@@ -34,7 +35,7 @@
                         <li class="list-group-item"><a href="{{ BASE_URL . '/brands-index' }}">Quản lí thương hiệu</a></li>
                         <li class="list-group-item"><a href="{{ BASE_URL . '/color-index' }}">Quản lí màu sắc</a></li>
                         <li class="list-group-item"><a href="{{ BASE_URL . '/size-index' }}">Quản lí size</a></li>
-                        <li class="list-group-item"><a href="{{ BASE_URL . '/brands-index' }}">Quản lí sản phẩm</a></li>
+                        <li class="list-group-item"><a href="{{ BASE_URL . '/products-index' }}">Quản lí sản phẩm</a></li>
                     </ul>
                 </nav>
             </div>
@@ -47,6 +48,7 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="./resource/js/app.js"></script>
 </body>
 
 </html>
